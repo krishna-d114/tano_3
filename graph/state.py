33eb2cf4ecef0dc48c2213be3c5c83b7ca6fcaf5
KrishnA <krisdevd.54@gmail.com>
2026-07-14 17:28:45 +0530

    done with state.py

---
+++ graph/state.py
@@ -0,0 +1,41 @@
+from typing import TypedDict, List,Dict,Optional
+from pydantic import Basemodel, Field
+
+class Short Metrics(BaseModel):
+    title : str = Field(description = "Title of the video")
+    views : str = Field(description = "view count ")
+    likes : str = Field(description = "likes count")
+    comments : str = Field(description = "comments count")
+    url : str = Field(description = "URL of the short")
+
+
+class CreatorProfie(BaseModel):
+    channel_id: str
+    channel_name:str
+    subscribers:int = 0
+    total_views: int = 0
+    video_count:int = 0
+    country :str = "N/A"
+    bio:str = ""
+    avg_views_per_video:int = 0
+    engagement_rate:float = 0.0
+    recent_shorts:List[ShortMetrics] = []
+    matched_keywords:List[str] = []
+    llm_status:str = "pending"
+    llm_reasoning:str = ""
+
+class CreatorFinderState(TypedDict):
+    product_description:str
+    budget_tier: str
+    location:str
+
+    keywords:List[str]
+    current_keyword_idx:int
+
+    current_batch_channel_ids: List[str]
+
+    candidates: Dict[str,CreatorProfile]
+
+    shortlist:List[CreatorProfile]
+    error:str
+    
